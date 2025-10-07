3fd2f340e17df648ede5ecf3e3697f8ff52f9689
huynkien <trungkienh524@gmail.com>
2025-10-07 22:53:08 +0700

    Exercise 10

---
+++ Core/Inc/matrix_animation.h
@@ -0,0 +1,25 @@
+/*
+ * matrix_animation.h
+ *
+ *  Created on: Oct 7, 2025
+ *      Author: Dell
+ */
+
+#ifndef INC_MATRIX_ANIMATION_H_
+#define INC_MATRIX_ANIMATION_H_
+
+#include "main.h"
+//#include "software_timer.h"
+
+extern uint8_t index_led_matrix;
+
+extern uint8_t matrix_buffer[8];
+
+void enableRow(uint8_t row);
+void displayRow(uint8_t matrix_buffer);
+void updateMatrix(uint8_t index);
+void displayMatrix();
+void ledAnimation();
+void Ex_run();
+
+#endif /* INC_MATRIX_ANIMATION_H_ */
