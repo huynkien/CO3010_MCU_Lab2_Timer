677483cb0a85f3ef0912e7173a1cafa3f9545f0a
huynkien <trungkienh524@gmail.com>
2025-10-07 21:01:45 +0700

    Exercise3 fixed

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -345,7 +345,7 @@ static void MX_GPIO_Init(void)
 /* USER CODE BEGIN 4 */
 uint8_t counter = 100;
 uint8_t counter_dot = 50;
-uint8_t counter_7seg = 25;
+uint8_t counter_7seg = 50;
 void HAL_TIM_PeriodElapsedCallback (TIM_HandleTypeDef *htim)
 {
 	counter--;
@@ -357,10 +357,8 @@ void HAL_TIM_PeriodElapsedCallback (TIM_HandleTypeDef *htim)
 	}
 	if (counter_dot <= 0) {
 		counter_dot = 50;
+		counter_7seg = 50;
 		HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
-	}
-	if (counter_7seg <= 0) {
-		counter_7seg = 25;
 		Ex_run();
 	}
 
